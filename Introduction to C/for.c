f111270ebf600ef8b6c8369992facde4bc27b265
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-10-21 12:43:27 +0500

    loops

---
+++ Introduction to C/for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i <= 5; i++)
+    {
+        /* code */
+        printf("i is: %d\n", i);
+    }
+    
+    return 0;
+}
---
+++ Introduction to C/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int condition = 0;
+    while (condition <= 5)
+    {
+        /* code */
+        printf("Condition is: %d\n", condition);
+        condition++;
+    }
+    
+    return 0;
+}
