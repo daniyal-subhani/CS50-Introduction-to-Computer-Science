735676e71ec14f1085565b56c564b8aef047b2e8
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-10-21 13:47:03 +0500

    loops

---
+++ Introduction to C/cat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Type a number: ");
+    scanf("%d", &number);
+    for (int i = 0; i <= number; i++)
+    {
+        /* code */
+        printf("meow \n");
+    }
+    
+    return 0;
+}
